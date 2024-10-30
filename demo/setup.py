5a7a77f388a4f51af62779f91d46d7cd06efa733
Joshua Hall <jch7aa@virginia.edum>
2024-10-29 21:16:39 -0400

    class excercise

---
+++ demo/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/JC-Hall/M09-demo-5100.git',
+    author='Josh',
+    author_email='jch7aa@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
+
